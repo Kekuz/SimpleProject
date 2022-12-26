4f9ac0a2cf05bb22b7d8243a310ce99387072b28
Kekuz <anton.edlenko@yandex.ru>
2022-12-26 22:10:54 +0300

    Tests upd

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,8 @@
-#x = int(input())
-x = 1.1
-res = 1
-for i in range(1, x + 1):
-    res *= i
-print(res)
+def factorial(x):
+    res = 1
+    for i in range(1, x + 1):
+        res *= i
+    return(res)
+
+if __name__ == '__main__':
+    print(factorial(2))
---
+++ Tests.py
@@ -0,0 +1,12 @@
+from Factorial import factorial
+
+def test_fac_1():
+    assert factorial(1) == 1
+
+
+def test_fac_2():
+    assert factorial(2) == 2
+
+
+def test_fac_7():
+    assert factorial(7) == 5040
